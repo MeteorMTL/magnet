abbfe1370ec158418a4fa0fd42fb2d1598c4f51c
Paul Cuciureanu <paulcu@gmail.com>
2015-05-30 12:57:56 -0400

    Adding additional organizers

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,5 +1,5 @@
 Meteor.startup(function () {
-  var users = Meteor.users.find({emails: {$elemMatch: {address: {$in: ["davidrowley01@gmail.com", "paulcu@gmail.com"]}}}});
+  var users = Meteor.users.find({emails: {$elemMatch: {address: {$in: ["davidrowley01@gmail.com", "paulcu@gmail.com", "tzumby@gmail.com", "tzumby@gmail.com", "calincu@gmail.com"]}}}});
   users.forEach(function (user){
     Roles.addUsersToRoles(user._id, ['organizer']);
   });
