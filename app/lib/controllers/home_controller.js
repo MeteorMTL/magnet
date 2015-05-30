15be5bc69da5481c9339fcb025dde9586ffe0399
Paul Cuciureanu <paulcu@gmail.com>
2015-05-30 18:36:07 -0400

    Fixing styles

--- app/lib/controllers/home_controller.js
+++ app/lib/controllers/home_controller.js
@@ -1,10 +1,12 @@
 HomeController = RouteController.extend({
   layoutTemplate: 'MasterLayout',
+  subscriptions: function () {
+    this.subscribe('Photos').wait();
+  },
   waitOn: function() {
     return [
       Meteor.subscribe('Votes'),
       Meteor.subscribe('Topics'),
-      Meteor.subscribe('Photos'),
       Meteor.subscribe('UserData'),
     ];
   },
