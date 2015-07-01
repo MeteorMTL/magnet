56b7a4242b4e3ac292a9d26e4e5c8187d3db0709
Paul Cuciureanu <paulcu@gmail.com>
2015-07-01 13:13:13 -0400

    Adding footer with email

--- app/lib/dictionary.js
+++ app/lib/dictionary.js
@@ -2,3 +2,13 @@ orion.dictionary.addDefinition('name', 'site', {
   type: String,
   label: "Site Name"
 });
+
+orion.dictionary.addDefinition('email', 'site', {
+  type: String,
+  label: "Site Name"
+});
+
+orion.dictionary.addDefinition('company.name', 'site', {
+  type: String,
+  label: "Company Name"
+});
